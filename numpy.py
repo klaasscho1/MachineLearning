cf11f20fe47939b1db748e82233069ca82982b4a
Klaas Schoenmaker <klaas.schoenmaker@student.auc.nl>
2017-09-12 12:29:44 +0200

    Numpy exercises

---
+++ numpy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 11:58:41 2017
+
+@author: klaasschoenmaker
+"""
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+arr *= 2
+
+for a in arr:
+    print(a)
